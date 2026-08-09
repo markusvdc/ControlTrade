cf44976170c46e1800d964597e6e19c01786e59f
Markus Domenegheti <markusvdc@gmail.com>
2026-08-09 04:09:51 -0300

    Make unreachable riders expose their horses

---
+++ src/main/java/br/com/smarttrade/mixin/MeleeAttackGoalInsaneDifficultyMixin.java
@@ -0,0 +1,49 @@
+package br.com.smarttrade.mixin;
+
+import br.com.smarttrade.gameplay.InsaneDifficulty;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.animal.equine.AbstractHorse;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MeleeAttackGoal.class)
+public abstract class MeleeAttackGoalInsaneDifficultyMixin {
+	@Shadow
+	@Final
+	protected PathfinderMob mob;
+
+	@Shadow
+	protected abstract boolean isTimeToAttack();
+
+	@Shadow
+	protected abstract void resetAttackCooldown();
+
+	@Inject(method = "checkAndPerformAttack", at = @At("HEAD"), cancellable = true)
+	private void smarttrade$attackHorseWhenRiderIsOutOfReach(LivingEntity target, CallbackInfo callback) {
+		if (
+			InsaneDifficulty.isActive(this.mob.level())
+				&& target instanceof Player player
+				&& player.getVehicle() instanceof AbstractHorse horse
+				&& this.mob.level() instanceof ServerLevel serverLevel
+				&& horse.isAlive()
+				&& !this.mob.isWithinMeleeAttackRange(player)
+				&& this.mob.isWithinMeleeAttackRange(horse)
+				&& this.mob.getSensing().hasLineOfSight(horse)
+				&& this.isTimeToAttack()
+		) {
+			this.resetAttackCooldown();
+			this.mob.swing(InteractionHand.MAIN_HAND);
+			this.mob.doHurtTarget(serverLevel, horse);
+			callback.cancel();
+		}
+	}
+}
